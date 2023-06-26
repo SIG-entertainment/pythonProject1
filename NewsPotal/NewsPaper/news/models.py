5a4232adfda9a9bd3a45470da2aff2678db8b9a9
SIG-entertainment <lupene@yandex.ru>
2023-06-26 10:43:51 +0300

    test1212

--- NewsPotal/NewsPaper/news/models.py
+++ NewsPotal/NewsPaper/news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Order(models.Model):
+    pass
+
+class Product(models.Model):
+    name = models.CharField(max_length=255),
+    price = models.FloatField(default=0.0)
+
+class Staff(models.Model):
+    pass
+class ProductOrder(models.Model):
+    pass
+
 # Create your models here.
